c7fbdc31f44307fe1d596f3c18c229bfb2ea1b48
孤风 <129588354+gengfeng520@users.noreply.github.com>
2023-04-07 14:47:59 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -18,7 +18,7 @@ const BLOG = {
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '', // 邮箱地址 例如mail@tangly1024.com
-  CONTACT_WEIXIN: process.env.NEXT_PUBLIC_CONTACT_WEIXIN|| 'litianbug'🔞, // 你的微信账号
+  CONTACT_WEIXIN: process.env.NEXT_PUBLIC_CONTACT_WEIXIN|| 'litianbug🔞', // 你的微信账号
   CONTACT_TWITTER: process.env.NEXT_PUBLIC_CONTACT_TWITTER || 'https://twitter.com/gufeng520131', // 你的twitter个人主页
   CONTACT_GITHUB: process.env.NEXT_PUBLIC_CONTACT_GITHUB || 'https://github.com/gengfeng520', // 你的github个人主页 例如 https://github.com/tangly1024
   CONTACT_TELEGRAM: process.env.NEXT_PUBLIC_CONTACT_TELEGRAM || '', // 你的telegram 地址 例如 https://t.me/tangly_1024
